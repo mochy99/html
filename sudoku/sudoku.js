d6d9507a22313bc35af9a327fb6f2ea9fb1a818b
mochy99 <mochy@Thaos-Air.lan>
2023-03-30 08:16:59 -0700

    fixed the match function

--- sudoku/sudoku.js
+++ sudoku/sudoku.js
@@ -1,5 +1,5 @@
-let sudoku = new Array(81);
-sudoku.unshift(5,3,4,6,7,8,9,1,2,
+const board = [
+    5,3,4,6,7,8,9,1,2,
     6,7,2,1,9,5,3,4,8,
     1,9,8,3,4,2,5,6,7,
     8,5,9,7,6,1,4,2,3,
@@ -7,8 +7,9 @@ sudoku.unshift(5,3,4,6,7,8,9,1,2,
     7,1,3,9,2,4,8,5,6,
     9,6,1,5,3,7,2,8,4,
     2,8,7,4,1,9,6,3,5,
-    3,4,5,2,8,6,1,7,9);
-let arrayLength = sudoku.length;
+    3,4,5,2,8,6,1,7,9
+];
+let sudoku;
 let randomIndex = [];
 let timerEl = document.getElementById("timer");
 let startEl = document.getElementById("startGame");
@@ -22,28 +23,29 @@ let submitEl = document.getElementById("submit");
 let value = "";
 let index = "";
 
-// function start game button
+// function START game button
 startEl.addEventListener("click", startbtn);
 function startbtn () {
-   for (let i = 0; i<cellEl.length; i++) {
-        if (cellEl.innerText !== ""){
-            cellEl[i].innerText = "";
-            cellEl[i].style.backgroundColor = "white";
-        }
-       };
+    sudoku = board;
+    console.log(board.length);
+    console.log(sudoku.length);
+    // clear the board
+    clearEl ();
     //push number generated randomly
     randomIndex = Array.of();
     randomIndex = randomList();
 
     randomIndex.forEach((element) => {
         cellEl[element].innerText = sudoku[element];
-       });
-    // resent the number of hint
+    });
+    console.log(board.length);
+    console.log(sudoku.length);
+    // refresh the number of hint
     numberEl.innerHTML = 3;
     //function timer
 }
 
-//function hint button
+//function HINT button
 hintEl.addEventListener("click", hint);
 function hint (){
     if (Number(numberEl.innerHTML) !== 0){
@@ -54,76 +56,84 @@ function hint (){
     } else {
         alert("You used all hints!");
     }
+    console.log(board.length);
+    console.log(sudoku.length);
 }
-// function submit button
+
+// function SUBMIT button
 submitEl.addEventListener("click", check);
 let i = 0;
 let acc = true;
 function check (){
+    changeCell ();
     let list = generateArray();
+    listNumber = Array.from(list, x => Number(x));
 
-    if ( list.every(checkFill)){
-        if (match(sudoku, list, 0)){
+    if ( listNumber.every(checkFill)){
+        if (match(sudoku,listNumber)){
             alert("You win!");
         } else {
             alert("You lose!");
         }
+        clearEl ();
     } else {
         alert("You need to fill all the blanks!");
     }
+    // check filled board?
     function checkFill(element) {
         return element > 0 && element < 10;
-      }
+    }
+    console.log(sudoku);
+    console.log(board);
+    console.log(board.length);
+    console.log(sudoku.length);
 }
+
 // function get value in entry row
 entryEl.addEventListener("click", getValue);
 function getValue (event){
     value = event.target.innerText;
 }
-// function generate the valid board
+
 // function fill a number in the blank
 tableEl.addEventListener("click", fillBlank);
 function fillBlank (eve) {
     let cell = eve.target;
-    if (cell.getElementsByClassName)
-     if ( cell.innerText === "" && value === ""){
-        alert("Please click the value in the last row that you want to insert!");
-    } else if (cell.innerText !== "" && value === "" ) {
-        cell.innerText = "";
-        cell.style.backgroundColor = "white";
-    } else if (value !== "") {
-        cell.innerText = value;
-        cell.style.backgroundColor = "grey";
-    } 
-    value = "";
-    
+    nameCell = cell.getAttribute("class");
+    if (nameCell === "cell"){
+        if ( cell.innerText === "" && value === ""){
+            alert("Please click the value in the last row that you want to insert!");
+        }  else if (cell.innerText !== "" && value === "" ) {
+            cell.innerText = "";
+            cell.style.backgroundColor = "white";
+        } else if (value !== "") {
+            cell.innerText = value;
+            cell.style.backgroundColor = "grey";
+        } 
+        value = "";
+    } else {
+        alert("You cannot change this value!"); 
+    }    
 }
 
 // function match 2 array
-function match (array1, array2, acc){
-    if (Array.isArray(array1) && Array.isArray(array2) ){
-        if (array1[0] == Number(array2[0])){
-            acc = true; 
-            newArray1 = array1.pop();
-            newArray2 = array2.pop();
-            return match (newArray1, newArray2, acc);      
-        } else {
-            return acc = false;
-        }
+function match (array1, array2){
+    if (array1.length == 0 && array2.length == 0 ){
+        return true;
     } else {
-        return acc;
-    }    
+        if (array1[0] == (array2[0])){
+            array1.shift();
+            array2.shift();
+            return match (array1, array2);      
+        } else {
+            return false;
+        }
+    }
 }
-let ar1 = [1,92];
-ar1 = ar1.pop();
-console.log(ar1);
-let ar2 = [1,6];
-console.log(match (ar1, ar2, acc));
-
 
 // function random a list
 function randomList () {
-    for (i=0; i < 40; i++) {
+    for (i=0; i < 190; i++) {
         let lambda = Math.floor(Math.random ()* 81);
         if (!randomIndex.includes(lambda)){
             randomIndex.push(lambda);
@@ -131,14 +141,30 @@ function randomList () {
     }
     return randomIndex.sort();
 }
+
 // function generate the array of cell
 function generateArray (){
     let checkList = Array.of();
-    for (let i = 0; i<cellEl.length; i++) {
+    for (let i = 0; i<81; i++) {
         value = cellEl[i].innerText;
         checkList.push(value);
     }
     return checkList;
 }
 
+// function change fixedCell to cell class
+function changeCell (){
+    randomIndex.forEach((element) => {
+        cellEl[element].classList.replace("fixedCell", "cell");
+       });
+}
 
+// function clear the board
+function clearEl (){
+    for (let i = 0; i<81; i++){
+        if (cellEl.innerText !== ""){
+            cellEl[i].innerText = "";
+            cellEl[i].style.backgroundColor = "white";
+        }
+    }
+}
